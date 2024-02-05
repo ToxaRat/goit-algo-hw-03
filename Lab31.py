ece55fd76ec2255d5667fe55c37d6ae5a095a9d6
ToxaRat <toxarat@gmail.com>
2024-02-05 13:32:51 +0200

    Перше завдання

--- Lab31.py
+++ Lab31.py
@@ -1,2 +1,28 @@
-#11
-#22
+from datetime import datetime
+
+def get_days_from_today(datestr):
+    try:
+        # Перетворіть рядок дати у форматі 'YYY-MM-ДД' у об'єкт datetime
+        datetime_object = datetime.strptime(datestr, "%Y-%m-%d")
+    except ValueError:
+        print(f"дата не у форматі 'YYY-MM-ДД'")
+        return None
+    # поточна дата
+    now = datetime.today()
+    # Розрахунок кількості днів
+    days_since = now.toordinal() - datetime_object.toordinal()
+    # Поверніть різницю у днях як ціле число
+    return days_since
+
+# з приклада
+razdate = get_days_from_today("2021-10-09")
+print(f"Різниця у днях: {razdate}")
+# попередній місяц
+razdate = get_days_from_today("2024-01-31")
+print(f"Різниця у днях: {razdate}")
+# наступний місяц
+razdate = get_days_from_today("2024-03-01")
+print(f"Різниця у днях: {razdate}")
+# невірний формат!!!
+razdate = get_days_from_today("2024.03.01")
+print(f"Різниця у днях: {razdate}")
