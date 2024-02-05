96dd424018bc433fb8941218caef4f064f3351ff
ToxaRat <toxarat@gmail.com>
2024-02-05 16:53:39 +0200

    4

---
+++ Lab34.py
@@ -0,0 +1,12 @@
+# Четверте завдання Модуль 3
+from datetime import datetime
+
+def get_upcoming_birthdays():
+    pass
+
+
+
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
