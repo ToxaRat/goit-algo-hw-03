db4a3a741863c5a6f9d2b6c7eac4cb17ed8cff70
ToxaRat <toxarat@gmail.com>
2024-02-09 10:50:01 +0200

    6

--- Lab32.py
+++ Lab32.py
@@ -2,14 +2,17 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    if isinstance(min, int)==False or isinstance(max, int)==False or isinstance(quantity, int)==False:
-        print(f"{min}, {max}, {quantity} - повинні бути числами!")
-        return None
-    elif min > max:
-        print(f"Перший парамет: {min} повинно буте менш другого: {max}!")
-        return None
     #  створюємр список
     numbersLot = list()
+    if isinstance(min, int)==False or isinstance(max, int)==False or isinstance(quantity, int)==False:
+        print(f"{min}, {max}, {quantity} - повинні бути числами!")
+        return numbersLot
+    elif min > max:
+        print(f"Перший парамет: {min} повинно буте менш другого: {max}!")
+        return numbersLot
+    elif (max-min+1) < quantity:
+        print(f"список унікальних чисел {quantity} для заданого діапазону ({min}–{max}) недостатній!")
+        return numbersLot
     # цикл 
     while True:
         # рандомне число
@@ -34,4 +37,7 @@ lottery_numbers = get_numbers_ticket("f", 49, 6)
 print("Ваші лотерейні числа у діапазоні від 1 до 49:", lottery_numbers)
 # Третий раз - некоректні дані
 lottery_numbers = get_numbers_ticket(10, 4, 5)
+print("Ваші лотерейні числа у діапазоні від 1 до 49:", lottery_numbers)
+# І на таких (10,14,6) вхідних даних програма взагалі не працює
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа у діапазоні від 1 до 49:", lottery_numbers)
